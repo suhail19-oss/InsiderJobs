03b6c354bdc7e19a06f300a85199e8ca7ce472d9
suhail19-oss <22je0584@iitism.ac.in>
2026-01-23 02:52:34 +0530

    Implement user data retrieval and job application functionality with error handling

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,6 +1,73 @@
-export const getUserData = async (req, res) => {};
+import JobApplication from "../models/jobapplication.js";
+import User from "../models/user.js";
+import Job from "../models/job.js";
 
-export const applyForJob = async (req, res) => {};
+export const getUserData = async (req, res) => {
+  const userId = req.auth.userId;
+
+  try {
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User Not Found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const applyForJob = async (req, res) => {
+  const { jobId } = req.body;
+  const userId = req.auth.userId;
+
+  try {
+    const isAlreadyApplied = await JobApplication.find({ jobId, userId });
+
+    if (isAlreadyApplied.length > 0) {
+      return res.status(409).json({
+        success: false,
+        message: "Already Applied",
+      });
+    }
+
+    const jobData = await Job.findById(jobId);
+
+    if (!jobData) {
+      return res.status(404).json({
+        success: false,
+        message: "Job Not Found",
+      });
+    }
+
+    await JobApplication.create({
+      companyId: jobData.companyId,
+      userId,
+      jobId,
+      date: Date.now(),
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Applied Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 export const getUserJobApplications = async (req, res) => {};
 
